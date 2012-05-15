fec264f7df7f8209433abe38e15725805c654d8f
Alexander Bothe <a_bothe@gmx.net>
2012-05-16 00:03:07 +0200

    .

---
+++ DParser2.Unittest/EvaluationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParserTests
+{
+	public class EvaluationTests
+	{
+		public static void Run()
+		{
+			Console.WriteLine("~ Evaluation tests...");
+
+			var dic = new Dictionary<string, object>
+			{
+				// Primary expressions
+				{"null",null},
+				{"1",(int)1},
+				{"-1",-1},
+				{"1.2",1.2},
+				{"'a'",'a'},
+				{"\"derp\"","derp"},
+				{"true",true},
+				{"false",false},
+			};
+
+			foreach (var kv in dic)
+				Check(kv.Key, kv.Value);
+		}
+
+		public static void Check(string code, object compValue)
+		{
+			var x = D_Parser.Parser.DParser.ParseExpression(code);
+
+			var v = D_Parser.Evaluation.ExpressionEvaluator.Evaluate(x, null);
+			
+			if (v == null || v.Value == null)
+			{
+				Console.WriteLine();
+				Console.WriteLine(code + "  returned null");
+			}
+			else if (!v.Value.Equals(compValue))
+			{
+				Console.WriteLine();
+				Console.WriteLine(code + "  -->");
+				Console.WriteLine(v.Value + " [evaluated]");
+				Console.WriteLine(compValue.ToString() + " [target value]");
+			}
+		}
+	}
+}
---
+++ DParser2.Unittest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using HighPrecisionTimer;
+using System.Threading;
+using D_Parser;
+using D_Parser.Dom;
+using D_Parser.Resolver;
+using D_Parser.Parser;
+
+namespace ParserTests
+{
+	class Program
+	{
+		public static string curFile = @"A:\D\dmd2\src\phobos\std\range.d";
+		public static void Main(string[] args)
+		{
+			//FormatterTest.RunTests();
+			//ParseTests.TestSourcePackages(false);
+			EvaluationTests.Run();
+
+			Console.WriteLine("~ Tests finished");
+			Console.ReadKey();
+		}
+
+		static void a()
+		{
+			var fcon=File.ReadAllText(curFile);
+			var lx = new Lexer(new StringReader(fcon));
+
+			var hp = new HighPrecTimer();
+
+			hp.Start();
+
+			lx.NextToken();
+
+			while (lx.LookAhead.Kind != DTokens.EOF)
+			{
+				lx.NextToken();
+			}
+
+			hp.Stop();
+
+			Console.WriteLine(Math.Round(hp.Duration, 3) + "s");
+		}
+
+		static void b()
+		{
+			string input =
+@"is(T[0] == This[]) && is(T==class) && is(T[0] T)";
+
+			//var mod = ParseTests.TestCode(input);
+			//ParseTests.TestExpression(input);
+			//ParseTests.TestExpressionStartFinder(input);
+		}
+
+		static void c()
+		{
+			string input = "";
+			while (true)
+			{
+				input = Console.ReadLine();
+
+				if (input == "q")
+					return;
+
+				var code = input;
+
+				ParseTests.TestMathExpression(code);
+			}
+		}
+
+		static void d()
+		{
+			ParseTests.TestSingleFile(curFile,true, false);
+		}
+	}
+}
