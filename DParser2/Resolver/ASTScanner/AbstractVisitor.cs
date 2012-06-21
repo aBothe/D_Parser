be6e50ed9f7138d2e0b6e8c0af84e097fec6a824
Alexander Bothe <a_bothe@gmx.net>
2012-06-21 23:21:17 +0200

    Also show const & enum values when listing all type members though they are not explicitly marked as static

--- DParser2/Resolver/ASTScanner/AbstractVisitor.cs
+++ DParser2/Resolver/ASTScanner/AbstractVisitor.cs
@@ -192,7 +192,7 @@ to avoid op­er­a­tions which are for­bid­den at com­pile time.",
 
 					// Add static and non-private members of all base classes; 
 					// Add everything if we're still handling the currently scoped class
-					if ((curWatchedClass == cls || dm2.IsStatic || !dm2.ContainsAttribute(DTokens.Private)) &&
+					if ((curWatchedClass == cls || dm2.IsStatic || (!(m is DVariable) || ((DVariable)dm2).IsConst) || !dm2.ContainsAttribute(DTokens.Private)) &&
 						(breakOnNextScope = HandleItem(m)) &&
 						breakImmediately)
 						return true;
