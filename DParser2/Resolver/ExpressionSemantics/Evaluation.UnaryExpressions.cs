996ab3dd96a5b6555c27b1839bc8a17971239b48
Alexander Bothe <a_bothe@gmx.net>
2012-07-21 04:21:48 +0200

    Began with unary expression evaluations.

--- DParser2/Resolver/ExpressionSemantics/Evaluation.UnaryExpressions.cs
+++ DParser2/Resolver/ExpressionSemantics/Evaluation.UnaryExpressions.cs
@@ -13,131 +13,196 @@ namespace D_Parser.Resolver.ExpressionSemantics
 	{
 		ISemantic E(UnaryExpression x)
 		{
-			if (x is UnaryExpression_Cat) // a = ~b;
-				return E((x as SimpleUnaryExpression).UnaryExpression);
-
-			else if (x is NewExpression)
-			{
-				// http://www.d-programming-language.org/expression.html#NewExpression
-				var nex = x as NewExpression;
-				ISemantic[] possibleTypes = null;
-
-				if (nex.Type is IdentifierDeclaration)
-					possibleTypes = TypeDeclarationResolver.Resolve((IdentifierDeclaration)nex.Type, ctxt, filterForTemplateArgs: false);
-				else
-					possibleTypes = TypeDeclarationResolver.Resolve(nex.Type, ctxt);
-
-				var ctors = new Dictionary<DMethod, ClassType>();
-
-				foreach (var t in possibleTypes)
-				{
-					if (t is ClassType)
-					{
-						var ct = (ClassType)t;
-
-						bool foundExplicitCtor = false;
-
-						foreach (var m in ct.Definition)
-							if (m is DMethod && ((DMethod)m).SpecialType == DMethod.MethodType.Constructor)
-							{
-								ctors.Add((DMethod)m, ct);
-								foundExplicitCtor = true;
-							}
-
-						if (!foundExplicitCtor)
-							ctors.Add(new DMethod(DMethod.MethodType.Constructor) { Type = nex.Type }, ct);
-					}
-				}
-
-				MemberSymbol finalCtor = null;
-
-				var kvArray = ctors.ToArray();
-
-				/*
-				 * TODO: Determine argument types and filter out ctor overloads.
-				 */
-
-				if (kvArray.Length != 0)
-					finalCtor = new MemberSymbol(kvArray[0].Key, kvArray[0].Value, x);
-				else if (possibleTypes.Length != 0)
-					return AbstractType.Get(possibleTypes[0]);
-
-				return finalCtor;
-			}
-
-
+			if (x is NewExpression)
+				return E((NewExpression)x);
 			else if (x is CastExpression)
-			{
-				var ce = x as CastExpression;
-
-				AbstractType castedType = null;
-
-				if (ce.Type != null)
-				{
-					var castedTypes = TypeDeclarationResolver.Resolve(ce.Type, ctxt);
-
-					ctxt.CheckForSingleResult(castedTypes, ce.Type);
-
-					if (castedTypes != null && castedTypes.Length != 0)
-						castedType = castedTypes[0];
-				}
-				else
-				{
-					castedType = AbstractType.Get(E(ce.UnaryExpression));
-
-					if (castedType != null && ce.CastParamTokens != null && ce.CastParamTokens.Length > 0)
-					{
-						//TODO: Wrap resolved type with member function attributes
-					}
-				}
-
-				return castedType;
-			}
-
-			else if (x is UnaryExpression_Add ||
-				x is UnaryExpression_Decrement ||
-				x is UnaryExpression_Increment ||
-				x is UnaryExpression_Sub ||
-				x is UnaryExpression_Not ||
-				x is UnaryExpression_Mul)
-				return E((x as SimpleUnaryExpression).UnaryExpression);
-
+				return E((CastExpression)x);
+			else if (x is UnaryExpression_Cat)
+				return E((UnaryExpression_Cat)x);
+			else if (x is UnaryExpression_Increment)
+				return E((UnaryExpression_Increment)x);
+			else if (x is UnaryExpression_Decrement)
+				return E((UnaryExpression_Decrement)x);
+			else if (x is UnaryExpression_Add)
+				return E((UnaryExpression_Add)x);
+			else if (x is UnaryExpression_Sub)
+				return E((UnaryExpression_Sub)x);
+			else if (x is UnaryExpression_Not)
+				return E((UnaryExpression_Not)x);
+			else if (x is UnaryExpression_Mul)
+				return E((UnaryExpression_Mul)x);
 			else if (x is UnaryExpression_And)
-				// &i -- makes an int* out of an int
-				return new PointerType(AbstractType.Get(E((x as UnaryExpression_And).UnaryExpression)), x);
+				return E((UnaryExpression_And)x);
 			else if (x is DeleteExpression)
-				return null;
+				return E((DeleteExpression)x);
 			else if (x is UnaryExpression_Type)
+				return E((UnaryExpression_Type)x);
+
+			return null;
+		}
+
+		ISemantic E(NewExpression nex)
+		{
+			// http://www.d-programming-language.org/expression.html#NewExpression
+			ISemantic[] possibleTypes = null;
+
+			if (nex.Type is IdentifierDeclaration)
+				possibleTypes = TypeDeclarationResolver.Resolve((IdentifierDeclaration)nex.Type, ctxt, filterForTemplateArgs: false);
+			else
+				possibleTypes = TypeDeclarationResolver.Resolve(nex.Type, ctxt);
+
+			var ctors = new Dictionary<DMethod, ClassType>();
+
+			foreach (var t in possibleTypes)
 			{
-				var uat = x as UnaryExpression_Type;
-
-				if (uat.Type == null)
-					return null;
-
-				var types = TypeDeclarationResolver.Resolve(uat.Type, ctxt);
-				ctxt.CheckForSingleResult(types, uat.Type);
-
-				if (types != null && types.Length != 0)
+				if (t is ClassType)
 				{
-					var id = new IdentifierDeclaration(uat.AccessIdentifier) { EndLocation = uat.EndLocation };
+					var ct = (ClassType)t;
 
-					// First off, try to resolve static properties
-					var statProp = StaticPropertyResolver.TryResolveStaticProperties(types[0], uat.AccessIdentifier, ctxt, eval, id);
+					bool foundExplicitCtor = false;
 
-					if (statProp != null)
-						return statProp;
+					foreach (var m in ct.Definition)
+						if (m is DMethod && ((DMethod)m).SpecialType == DMethod.MethodType.Constructor)
+						{
+							ctors.Add((DMethod)m, ct);
+							foundExplicitCtor = true;
+						}
 
-					// If it's not the case, try the conservative way
-					var res = TypeDeclarationResolver.Resolve(id, ctxt, types);
-
-					ctxt.CheckForSingleResult(res, x);
-
-					if (res != null && res.Length != 0)
-						return res[0];
+					if (!foundExplicitCtor)
+						ctors.Add(new DMethod(DMethod.MethodType.Constructor) { Type = nex.Type }, ct);
 				}
-
-				return null;
 			}
+
+			MemberSymbol finalCtor = null;
+
+			var kvArray = ctors.ToArray();
+
+			/*
+			 * TODO: Determine argument types and filter out ctor overloads.
+			 */
+
+			if (kvArray.Length != 0)
+				finalCtor = new MemberSymbol(kvArray[0].Key, kvArray[0].Value, nex);
+			else if (possibleTypes.Length != 0)
+				return AbstractType.Get(possibleTypes[0]);
+
+			return finalCtor;
+		}
+
+		ISemantic E(CastExpression ce)
+		{
+			AbstractType castedType = null;
+
+			if (ce.Type != null)
+			{
+				var castedTypes = TypeDeclarationResolver.Resolve(ce.Type, ctxt);
+
+				ctxt.CheckForSingleResult(castedTypes, ce.Type);
+
+				if (castedTypes != null && castedTypes.Length != 0)
+					castedType = castedTypes[0];
+			}
+			else
+			{
+				castedType = AbstractType.Get(E(ce.UnaryExpression));
+
+				if (castedType != null && ce.CastParamTokens != null && ce.CastParamTokens.Length > 0)
+				{
+					//TODO: Wrap resolved type with member function attributes
+				}
+			}
+
+			return castedType;
+		}
+
+		ISemantic E(UnaryExpression_Cat x) // a = ~b;
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_Increment x)
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_Decrement x)
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_Add x)
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_Sub x)
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_Not x)
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_Mul x)
+		{
+			return E(x.UnaryExpression);
+		}
+
+		ISemantic E(UnaryExpression_And x)
+		{
+			var ptrBase=E(x.UnaryExpression);
+
+			if (eval)
+			{
+				// Create a new pointer
+				// 
+			}
+
+			// &i -- makes an int* out of an int
+			return new PointerType(AbstractType.Get(ptrBase), x);
+		}
+
+		ISemantic E(DeleteExpression x)
+		{
+			if (eval)
+			{
+				// Reset the content of the variable
+			}
+
+			return null;
+		}
+
+		ISemantic E(UnaryExpression_Type x)
+		{
+			var uat = x as UnaryExpression_Type;
+
+			if (uat.Type == null)
+				return null;
+
+			var types = TypeDeclarationResolver.Resolve(uat.Type, ctxt);
+			ctxt.CheckForSingleResult(types, uat.Type);
+
+			if (types != null && types.Length != 0)
+			{
+				var id = new IdentifierDeclaration(uat.AccessIdentifier) { EndLocation = uat.EndLocation };
+
+				// First off, try to resolve static properties
+				var statProp = StaticPropertyResolver.TryResolveStaticProperties(types[0], uat.AccessIdentifier, ctxt, eval, id);
+
+				if (statProp != null)
+					return statProp;
+
+				// If it's not the case, try the conservative way
+				var res = TypeDeclarationResolver.Resolve(id, ctxt, types);
+
+				ctxt.CheckForSingleResult(res, x);
+
+				if (res != null && res.Length != 0)
+					return res[0];
+			}
+
 			return null;
 		}
 	}
