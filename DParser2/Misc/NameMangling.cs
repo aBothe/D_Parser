b59a64fd4eb1aabb2b3b447a4c06283abf286e01
Alexander Bothe <a_bothe@gmx.net>
2012-10-16 01:03:42 +0200

    Introduced name mangling helper class

---
+++ DParser2/Misc/NameMangling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D_Parser.Dom;
+using D_Parser.Resolver;
+
+namespace D_Parser.Misc
+{
+	public class NameMangling
+	{
+		public AbstractType Demangle(string mangledString, ResolverContextStack ctxt)
+		{
+			return null;
+		}
+
+		public static string Mangle(AbstractType typeToMangle)
+		{
+			return string.Empty;
+		}
+	}
+}
