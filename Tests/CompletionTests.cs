d16f05e40e19768f20dacc2b3105769567d62bca
aBothe <info@alexanderbothe.com>
2012-11-17 12:16:09 +0100

    Prepared a completion test stub

---
+++ Tests/CompletionTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using D_Parser;
+using D_Parser.Completion;
+using D_Parser.Dom;
+using D_Parser.Dom.Expressions;
+using D_Parser.Dom.Statements;
+using D_Parser.Misc;
+using D_Parser.Parser;
+using D_Parser.Resolver;
+using D_Parser.Resolver.ExpressionSemantics;
+using D_Parser.Resolver.Templates;
+using D_Parser.Resolver.TypeResolution;
+using NUnit.Framework;
+
+namespace Tests
+{
+	/// <summary>
+	/// Description of CompletionTests.
+	/// </summary>
+	public class CompletionTests
+	{
+		[Test]
+		public void ForStatement()
+		{
+			var code = @"module A; // 1
+void main(){
+	for(
+		int
+		i= // 5
+		0;
+		i<100;
+		i++)
+		
+	{ // 10
+	}";
+			var mod = DParser.ParseString(code);
+			var main = mod["main"][0] as DMethod;
+			
+			var l = new CodeLocation(1,4);
+			var off = DocumentHelper.LocationToOffset(code,l);
+			ParserTrackerVariables ptr;
+			var parsedBlock = CtrlSpaceCompletionProvider.FindCurrentCaretContext(code,main, off,l,out ptr);
+			
+			/*
+			 * a) Test if completion popup would open in each code situation
+			 * b) Test in which case iteration variables can be found.
+			 */
+			
+			//TODO
+		}
+	}
+}
