dcc171ee4d45166c2978e46d306eb224957b4be4
aBothe <info@alexanderbothe.com>
2013-01-17 20:18:44 +0100

    Ported the new indent engine to the stand-alone D_Parser.

--- DParser2/Formatting/DFormattingOptions.cs
+++ DParser2/Formatting/DFormattingOptions.cs
@@ -5,7 +5,38 @@ using System.Text;
 
 namespace D_Parser.Formatting
 {
+	public enum GotoLabelIndentStyle {
+		///<summary>Place goto labels in the leftmost column</summary>
+		LeftJustify,
+		
+		/// <summary>
+		/// Place goto labels one indent less than current
+		/// </summary>
+		OneLess,
+		
+		/// <summary>
+		/// Indent goto labels normally
+		/// </summary>
+		Normal
+	}
+	
+	public interface DFormattingOptionsFactory
+	{
+		DFormattingOptions Options{get;}
+	}
+	
 	public class DFormattingOptions
 	{
+		public bool IndentSwitchBody;
+		public GotoLabelIndentStyle LabelIndentStyle;
+		
+		public static DFormattingOptions CreateDStandard()
+		{
+			return new DFormattingOptions()
+			{
+				LabelIndentStyle = GotoLabelIndentStyle.OneLess,
+				IndentSwitchBody = true
+			};
+		}
 	}
 }
---
+++ DParser2/Formatting/Indent/IndentEngineWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using D_Parser.Dom;
+using D_Parser.Dom.Expressions;
+using D_Parser.Dom.Statements;
+using D_Parser.Resolver.TypeResolution;
+
+namespace D_Parser.Formatting.Indent
+{
+	public class IndentEngineWrapper
+	{
+		public static string CalculateIndent(string code, int line, bool tabsToSpaces = false, int indentWidth = 4)
+		{
+			using(var sr = new StringReader(code))
+				return CalculateIndent(sr, line, tabsToSpaces, indentWidth);
+		}
+		
+		public static string CalculateIndent(TextReader code, int line, bool tabsToSpaces = false, int indentWidth = 4)
+		{
+			if(line < 2)
+				return string.Empty;
+			
+			var eng = new IndentEngine(DFormattingOptions.CreateDStandard(), tabsToSpaces, indentWidth);
+			
+			int curLine = 1;
+			const int lf = (int)'\n';
+			const int cr = (int)'\r';
+			int c;
+			while((c = code.Read()) != -1)
+			{
+				if(c == lf || c == cr)
+				{
+					if(c == cr && code.Peek() == lf)
+						code.Read();
+					
+					if(++curLine > line)
+						break;
+				}
+				
+				eng.Push((char)c);
+			}
+			
+			return eng.NewLineIndent;
+		}
+	}
+}
