7ad6157f42e192b3c4c822773b1488cfe9908eb1
aBothe <info@alexanderbothe.com>
2013-01-19 14:37:50 +0100

    Slight performance improvement for custom formatting tasks

--- DParser2/Formatting/DocumentAdapter.cs
+++ DParser2/Formatting/DocumentAdapter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using D_Parser.Dom;
+using System.Collections.Generic;
 
 namespace D_Parser.Formatting
 {
@@ -14,10 +15,14 @@ namespace D_Parser.Formatting
 	}
 	
 	public class TextDocument : IDocumentAdapter
-	{
+	{		
 		string text = string.Empty;
-		public string Text{get{return text;} set{text = value;}}
+		public string Text{get{return text;} set{text = value; UpdateLineInfo();}}
 		public char this[int o] { get{ return text[o]; } }
+		/// <summary>
+		/// Contains the start offsets of each line
+		/// </summary>
+		int[] lines;
 		
 		public int TextLength {
 			get {
@@ -27,17 +32,71 @@ namespace D_Parser.Formatting
 		
 		public int ToOffset(CodeLocation loc)
 		{
-			return DocumentHelper.LocationToOffset(text, loc.Line, loc.Column);
+			return ToOffset(loc.Line, loc.Column);
 		}
 		
 		public int ToOffset(int line, int column)
 		{
-			return DocumentHelper.LocationToOffset(text,line, column);
+			if(line <= 0 || line > lines.Length)
+				throw new IndexOutOfRangeException("Line must be a value between 1 and "+lines.Length+"; Was "+line);
+			
+			return lines[line-1] + column - 1;
 		}
 		
 		public CodeLocation ToLocation(int offset)
 		{
-			return DocumentHelper.OffsetToLocation(text, offset);
+			if(text.Length == 0 || lines == null)
+				return CodeLocation.Empty;
+			else if(text.Length == 1)
+				return new CodeLocation(offset + 1, 1);
+			
+			// Primitive binary choice for better performance in huge files
+			if(offset < text.Length/2)
+			{
+				for(int i = 1; i < lines.Length; i++)
+				{
+					if(lines[i] > offset)
+						return new CodeLocation(offset - lines[i-1] + 1, i);
+				}
+				return new CodeLocation(offset - lines[lines.Length-1] + 1, lines.Length);
+			}
+			else
+			{
+				for(int i = lines.Length-1; i > 0; i--)
+				{
+					if(lines[i] < offset)
+						return new CodeLocation(offset - lines[i] + 1, i+1);
+				}
+				return new CodeLocation(offset + 1, 1);
+			}
+		}
+		
+		void UpdateLineInfo()
+		{
+			var l = new List<int>();
+			
+			int lastStart = 0;
+			int len = text == null ? 0 : text.Length;
+			for(int i = 0; i< len; i++)
+			{
+				if(text[i] == '\r')
+				{
+					if(i+1 < len && text[i+1] == '\n')
+						i++;
+					l.Add(lastStart);
+					lastStart = i+1;
+				}
+				else if(text[i] == '\n')
+				{
+					l.Add(lastStart);
+					lastStart = i+1;
+				}
+			}
+			
+			if(len > 0)
+				l.Add(lastStart);
+			
+			lines = l.ToArray();
 		}
 	}
 }
