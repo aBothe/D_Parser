d8d6a438b3a81a3495923936afde8b82476910d1
aBothe <info@alexanderbothe.com>
2013-11-25 07:17:55 +0100

    [Completion] Overwrite last method's body/in/out block in order to have new recently typed types/vars/methods etc. available immediately after typing.

--- DParser2/Completion/Providers/CtrlSpaceCompletionProvider.cs
+++ DParser2/Completion/Providers/CtrlSpaceCompletionProvider.cs
@@ -237,8 +237,35 @@ namespace D_Parser.Completion
 					else if (CurrentScope is DMethod)
 					{
 						psr.Step();
-						(CurrentScope as DMethod).Clear();
-						ret = psr.BlockStatement(CurrentScope);
+						var dm = CurrentScope as DMethod;
+						dm.Clear();
+
+						var methodRegion = DTokens.Body;
+
+						if (dm.In != null && blockStartLocation == dm.In.Location)
+							methodRegion = DTokens.In;
+
+						if (dm.Out != null && blockStartLocation == dm.Out.Location)
+							methodRegion = DTokens.Out;
+
+						var newBlock = psr.BlockStatement(CurrentScope);
+						ret = newBlock;
+
+						switch (methodRegion)
+						{
+							case DTokens.Body:
+								newBlock.EndLocation = dm.Body.EndLocation;
+								dm.Body = newBlock;
+								break;
+							case DTokens.In:
+								newBlock.EndLocation = dm.In.EndLocation;
+								dm.In = newBlock;
+								break;
+							case DTokens.Out:
+								newBlock.EndLocation = dm.Out.EndLocation;
+								dm.Out = newBlock;
+								break;
+						}
 					}
 					else if (CurrentScope is DModule)
 						ret = psr.Root();
