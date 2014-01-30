da0b1ce7208c7540bd564dfefcdc5d53c5ca3472
Orvid King <blah38621@gmail.com>
2014-01-30 11:40:53 -0600

    Formatted ModulePackage.cs

--- DParser2/Dom/ModulePackage.cs
+++ DParser2/Dom/ModulePackage.cs
@@ -9,21 +9,24 @@ namespace D_Parser.Dom
 {
 	public class MutableRootPackage : RootPackage
 	{
-		public MutableRootPackage() {}
+		public MutableRootPackage()
+		{
+		}
+
 		public MutableRootPackage(params DModule[] modules)
 		{
-			foreach(var m in modules)
+			foreach (var m in modules)
 				base.AddModule(m);
 		}
 
 		public new bool AddModule(DModule ast)
 		{
-			return base.AddModule (ast);
+			return base.AddModule(ast);
 		}
 
 		public new ModulePackage GetOrCreateSubPackage(string package, bool create = false)
 		{
-			return base.GetOrCreateSubPackage (package, create);
+			return base.GetOrCreateSubPackage(package, create);
 		}
 
 		public new bool RemovePackage(string name)
@@ -33,7 +36,7 @@ namespace D_Parser.Dom
 
 		public new bool RemoveModule(string name)
 		{
-			return base.RemoveModule (name);
+			return base.RemoveModule(name);
 		}
 	}
 
@@ -41,9 +44,12 @@ namespace D_Parser.Dom
 	{
 		internal DateTime LastParseTime;
 
-		public RootPackage() : base(null, "<root>") {}
+		public RootPackage() : base(null, "<root>")
+		{
+		}
 
 		#region Common type bypasses
+
 		public bool IsObjectClassDefined
 		{
 			get { return ObjectClass != null; }
@@ -52,53 +58,47 @@ namespace D_Parser.Dom
 		/// <summary>
 		/// To improve resolution performance, the object class that can be defined only once will be stored over here.
 		/// </summary>
-		public DClassLike ObjectClass
-		{
-			get;
-			private set;
-		}
+		public DClassLike ObjectClass { get; private set; }
 
-		public AbstractType SizeT
-		{
-			get;
-			private set;
-		}
+		public AbstractType SizeT { get; private set; }
 
 		/// <summary>
 		/// See <see cref="ObjectClass"/>
 		/// </summary>
-		public ClassType ObjectClassResult
-		{
-			get;
-			private set;
-		}
+		public ClassType ObjectClassResult { get; private set; }
 
 		public void TryPreResolveCommonTypes()
 		{
-			var obj = GetModule ("object");
+			var obj = GetModule("object");
 			if (obj == null)
 				return;
 
 			ParseCacheView cache = null;
 
-			foreach (var m in obj) {
-				if (m.Name == "Object" && m is DClassLike) {
-					ObjectClassResult = new ClassType (ObjectClass = (DClassLike)m, new IdentifierDeclaration ("Object"), null);
+			foreach (var m in obj)
+			{
+				if (m.Name == "Object" && m is DClassLike)
+				{
+					ObjectClassResult = new ClassType(ObjectClass = (DClassLike)m, new IdentifierDeclaration("Object"), null);
 					break;
-				} else if (m.Name == "size_t") {
+				}
+				else if (m.Name == "size_t")
+				{
 					if (cache == null)
-						cache = new ParseCacheView (new[]{this});
+						cache = new ParseCacheView(new[]{ this });
 					//TODO: Do a version check, so that only on x64 dmd installations, size_t equals ulong.
-					SizeT = TypeDeclarationResolver.HandleNodeMatch (m, 
-					                                                 ResolutionContext.Create (cache, null, obj));
+					SizeT = TypeDeclarationResolver.HandleNodeMatch(m, 
+						ResolutionContext.Create(cache, null, obj));
 				}
 			}
 		}
+
 		#endregion
 
 		public override RootPackage Root
 		{
-			get{
+			get
+			{
 				return this;
 			}
 		}
@@ -106,60 +106,64 @@ namespace D_Parser.Dom
 
 	public class ModulePackage : IEnumerable<DModule>, IEnumerable<ModulePackage>
 	{
-		internal ModulePackage(ModulePackage parent, string name) {
+		internal ModulePackage(ModulePackage parent, string name)
+		{
 			this.Parent = parent;
-			Strings.Add (name);
-			NameHash = name.GetHashCode ();
+			Strings.Add(name);
+			NameHash = name.GetHashCode();
 		}
 
 		public readonly ModulePackage Parent;
 
 		public virtual RootPackage Root
 		{
-			get{
+			get
+			{
 				return Parent != null ? Parent.Root : null;
 			}
 		}
 
-		public string Name {get{return Strings.TryGet (NameHash);}}
+		public string Name { get { return Strings.TryGet(NameHash); } }
+
 		public readonly int NameHash;
 		internal ConcurrentDictionary<int, ModulePackage> packages = new ConcurrentDictionary<int, ModulePackage>();
 		internal ConcurrentDictionary<int, DModule> modules = new ConcurrentDictionary<int, DModule>();
-		
-		public IEnumerable<KeyValuePair<int,ModulePackage>> Packages {get{return packages;}}
-		public IEnumerable<KeyValuePair<int, DModule>> Modules {get{return modules;}}
-		
-		public bool IsEmpty {get{return packages.Count == 0 && modules.Count == 0;}}
-		
+
+		public IEnumerable<KeyValuePair<int,ModulePackage>> Packages { get { return packages; } }
+
+		public IEnumerable<KeyValuePair<int, DModule>> Modules { get { return modules; } }
+
+		public bool IsEmpty { get { return packages.Count == 0 && modules.Count == 0; } }
+
 		public ModulePackage[] GetPackages()
 		{
 			var packs = new ModulePackage[packages.Count];
-			packages.Values.CopyTo(packs,0);
+			packages.Values.CopyTo(packs, 0);
 			return packs;
 		}
-		
+
 		public DModule[] GetModules()
 		{
 			var mods = new DModule[modules.Count];
-			modules.Values.CopyTo(mods,0);
+			modules.Values.CopyTo(mods, 0);
 			return mods;
 		}
-		
+
 		public ModulePackage GetPackage(string name)
 		{
-			return GetPackage (name.GetHashCode ());
+			return GetPackage(name.GetHashCode());
 		}
 
 		public ModulePackage GetPackage(int nameHash)
 		{
 			ModulePackage pack;
-			packages.TryGetValue(nameHash,out pack);
+			packages.TryGetValue(nameHash, out pack);
 			return pack;
 		}
 
 		public DModule GetModule(string name)
 		{
-			var pack = GetSubPackage (ModuleNameHelper.ExtractPackageName (name));
+			var pack = GetSubPackage(ModuleNameHelper.ExtractPackageName(name));
 			DModule ast;
 			if (pack == null)
 				return null;
@@ -196,8 +200,8 @@ namespace D_Parser.Dom
 				yield return kv.Value;
 
 			foreach (var kv in packages)
-					foreach (var ast in kv.Value)
-						yield return ast;
+				foreach (var ast in kv.Value)
+					yield return ast;
 		}
 
 		IEnumerator<ModulePackage> IEnumerable<ModulePackage>.GetEnumerator()
@@ -215,20 +219,20 @@ namespace D_Parser.Dom
 		{
 			return GetEnumerator();
 		}
-		
+
 		internal bool AddModule(DModule ast)
 		{
-			if(ast == null || string.IsNullOrEmpty(ast.ModuleName))
+			if (ast == null || string.IsNullOrEmpty(ast.ModuleName))
 				return false;
 
-			var pack = Root.GetOrCreateSubPackage (ModuleNameHelper.ExtractPackageName (ast.ModuleName), true);
+			var pack = Root.GetOrCreateSubPackage(ModuleNameHelper.ExtractPackageName(ast.ModuleName), true);
 
-			var modName = ModuleNameHelper.ExtractModuleName (ast.ModuleName);
-			Strings.Add (modName);
+			var modName = ModuleNameHelper.ExtractModuleName(ast.ModuleName);
+			Strings.Add(modName);
 			pack.modules[modName.GetHashCode()] = ast;
 			return true;
 		}
-		
+
 		internal bool RemovePackage(string name)
 		{
 			return RemovePackage(ModuleNameHelper.ExtractModuleName(name).GetHashCode());
@@ -239,30 +243,30 @@ namespace D_Parser.Dom
 			ModulePackage p;
 			return packages.TryRemove(nameHash, out p);
 		}
-		
+
 		internal bool RemoveModule(string name)
 		{
 			name = ModuleNameHelper.ExtractModuleName(name);
 			DModule ast;
-			return modules.TryRemove (name.GetHashCode (), out ast);
+			return modules.TryRemove(name.GetHashCode(), out ast);
 		}
 
 		public ModulePackage GetSubPackage(string package)
 		{
-			return GetOrCreateSubPackage (package, false);
+			return GetOrCreateSubPackage(package, false);
 		}
 
 		public DModule GetSubModule(string moduleName)
 		{
-			if (string.IsNullOrEmpty (moduleName))
+			if (string.IsNullOrEmpty(moduleName))
 				return null;
 
-			var pack = GetOrCreateSubPackage (ModuleNameHelper.ExtractPackageName (moduleName));
+			var pack = GetOrCreateSubPackage(ModuleNameHelper.ExtractPackageName(moduleName));
 
 			if (pack == null)
 				return null;
 
-			return pack.GetModule (ModuleNameHelper.ExtractModuleName(moduleName));
+			return pack.GetModule(ModuleNameHelper.ExtractModuleName(moduleName));
 		}
 
 		internal ModulePackage GetOrCreateSubPackage(string package, bool create = false)
@@ -276,11 +280,11 @@ namespace D_Parser.Dom
 			foreach (string part in parts)
 			{
 				ModulePackage returnValue;
-				var hash = part.GetHashCode ();
+				var hash = part.GetHashCode();
 				if (!currentPackage.packages.TryGetValue(hash, out returnValue))
 				{
 					if (create)
-						returnValue = currentPackage.packages [hash] = new ModulePackage (currentPackage, part);
+						returnValue = currentPackage.packages[hash] = new ModulePackage(currentPackage, part);
 					else
 						return null;
 				}
@@ -298,7 +302,7 @@ namespace D_Parser.Dom
 
 		public DModule this[string modName]
 		{
-			get{return GetModule (modName);}
+			get{ return GetModule(modName); }
 		}
 	}
 }
