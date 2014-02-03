1ee147d7ffc99b9f2365e6dd300f216e88a9872f
aBothe <info@alexanderbothe.com>
2014-02-03 01:01:21 +0100

    Began with internally optimizing all if(a is A) else if(a is B) cases to be switches instead.

--- DParser2/Dom/ISyntaxRegion.cs
+++ DParser2/Dom/ISyntaxRegion.cs
@@ -1,10 +1,15 @@
 ﻿
+using System;
+using System.Collections.Generic;
+using System.Threading;
 namespace D_Parser.Dom
 {
 	public interface ISyntaxRegion
 	{
 		CodeLocation Location { get; }
 		CodeLocation EndLocation { get; }
+
+		//int Kind { get; }
 	}
 
 	public static class SyntaxRegionHelper
@@ -12,6 +17,31 @@ namespace D_Parser.Dom
 		public static ISyntaxRegion First(this ISyntaxRegion a, ISyntaxRegion b)
 		{
 			return a.Location <= b.Location ? a : b;
+		}
+		
+		static int NextTypeKind = 0;
+		static Dictionary<Type, int> Kinds = new Dictionary<Type, int>();
+
+		public static int Kind(this ISyntaxRegion sr)
+		{
+			int k;
+			if (Kinds.TryGetValue(sr.GetType(), out k))
+				return k;
+
+			k = Interlocked.Increment(ref NextTypeKind);
+			Kinds[sr.GetType()] = k;
+			return k;
+		}
+
+		public static int Kind<T>()
+		{
+			int k;
+			if (Kinds.TryGetValue(typeof(T), out k))
+				return k;
+
+			k = Interlocked.Increment(ref NextTypeKind);
+			Kinds[typeof(T)] = k;
+			return k;
 		}
 	}
 }
