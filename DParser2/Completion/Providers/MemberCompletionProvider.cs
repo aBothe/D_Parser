9b768e81b138bc446bbef664e566b94d5b5f6e85
aBothe <info@alexanderbothe.com>
2014-02-22 05:03:31 +0100

    [Completion] Refactored member completion provider.

--- DParser2/Completion/Providers/MemberCompletionProvider.cs
+++ DParser2/Completion/Providers/MemberCompletionProvider.cs
@@ -12,7 +12,7 @@ using D_Parser.Resolver.TypeResolution;
 
 namespace D_Parser.Completion.Providers
 {
-	class MemberCompletionProvider : AbstractCompletionProvider
+	class MemberCompletionProvider : AbstractCompletionProvider, IResolvedTypeVisitor
 	{
 		ResolutionContext ctxt;
 		public ISyntaxRegion AccessExpression;
@@ -20,6 +20,9 @@ namespace D_Parser.Completion.Providers
 		public IBlockNode ScopedBlock;
 		public MemberFilter MemberFilter = MemberFilter.All;
 		IEditorData ed;
+
+		bool isVariableInstance;
+
 
 		public MemberCompletionProvider(ICompletionDataGenerator cdg, ISyntaxRegion sr, IBlockNode b, IStatement stmt) : base(cdg) {
 			AccessExpression = sr;
@@ -47,110 +50,191 @@ namespace D_Parser.Completion.Providers
 			if (t == null) //TODO: Add after-space list creation when an unbound . (Dot) was entered which means to access the global scope
 				return;
 
-			BuildCompletionData(t);
+			isVariableInstance = false;
+
+			if (t.DeclarationOrExpressionBase is ITypeDeclaration)
+				isVariableInstance |= (t.DeclarationOrExpressionBase as ITypeDeclaration).ExpressesVariableAccess;
+
+			t.Accept(this);
 		}
 
-		void BuildCompletionData(
-			AbstractType rr,
-			bool isVariableInstance = false,
-			AbstractType resultParent = null)
+		void GenUfcsAndStaticProperties(AbstractType t)
 		{
-			if (rr == null)
+			if(isVariableInstance && CompletionOptions.Instance.ShowUFCSItems)
+				foreach (var ufcsItem in UFCSResolver.TryResolveUFCS(t, 0, ed.CaretLocation, ctxt))
+					CompletionDataGenerator.Add ((ufcsItem as DSymbol).Definition);
+			StaticProperties.ListProperties(CompletionDataGenerator, MemberFilter, t, isVariableInstance);
+		}
+
+		public void VisitPrimitiveType(PrimitiveType pt)
+		{
+			GenUfcsAndStaticProperties(pt);
+		}
+
+		public void VisitPointerType(PointerType pt)
+		{
+			if (pt.Base != null &&
+				!(pt.Base is PrimitiveType && pt.Base.DeclarationOrExpressionBase is PointerDecl))
+			{
+				isVariableInstance = true;
+				pt.Base.Accept(this);
+			}
+			else
+				GenUfcsAndStaticProperties(pt);
+		}
+
+		public void VisitArrayType(ArrayType at)
+		{
+			VisitAssocArrayType(at);
+		}
+
+		public void VisitAssocArrayType(AssocArrayType aa)
+		{
+			isVariableInstance = true;
+			GenUfcsAndStaticProperties(aa);
+		}
+
+		public void VisitDelegateCallSymbol(DelegateCallSymbol dg)
+		{
+			isVariableInstance = true;
+			if (dg.Base != null)
+				dg.Base.Accept(this);
+			else
+				GenUfcsAndStaticProperties(dg);
+		}
+
+		public void VisitArrayAccessSymbol(ArrayAccessSymbol aas)
+		{
+			isVariableInstance = true;
+			if (aas.Base != null)
+				aas.Base.Accept(this);
+			else
+				GenUfcsAndStaticProperties(aas);
+		}
+
+		public void VisitDelegateType(DelegateType dg)
+		{
+			GenUfcsAndStaticProperties(dg);
+		}
+
+		public void VisitAliasedType(AliasedType at)
+		{
+			if (at.Base != null)
+				at.Base.Accept(this);
+			else
+				GenUfcsAndStaticProperties(at);
+		}
+
+		void VisitTemplateIntermediateType(TemplateIntermediateType tr)
+		{
+			if (tr.DeclarationOrExpressionBase is TokenExpression)
+			{
+				var token = ((TokenExpression)tr.DeclarationOrExpressionBase).Token;
+
+				isVariableInstance = token == DTokens.This || token == DTokens.Super;
+			}
+
+			// Cases:
+
+			// myVar. (located in basetype definition)		<-- Show everything
+			// this. 										<-- Show everything
+			// myVar. (not located in basetype definition) 	<-- Show public and public static members
+			// super. 										<-- Show all base type members
+			// myClass. (not located in myClass)			<-- Show all static members
+			// myClass. (located in myClass)				<-- Show all static members
+
+			MemberCompletionEnumeration.EnumChildren(CompletionDataGenerator, ctxt, tr, isVariableInstance, MemberFilter);
+
+			GenUfcsAndStaticProperties(tr);
+		}
+
+		public void VisitEnumType(EnumType en)
+		{
+			foreach (var e in en.Definition)
+				CompletionDataGenerator.Add(e);
+			// TODO: Enlist ufcs items&stat props here aswell?
+		}
+
+		public void VisitStructType(StructType t)
+		{
+			VisitTemplateIntermediateType(t);
+		}
+
+		public void VisitUnionType(UnionType t)
+		{
+			VisitTemplateIntermediateType(t);
+		}
+
+		public void VisitClassType(ClassType t)
+		{
+			VisitTemplateIntermediateType(t);
+		}
+
+		public void VisitInterfaceType(InterfaceType t)
+		{
+			VisitTemplateIntermediateType(t);
+		}
+
+		public void VisitTemplateType(TemplateType t)
+		{
+			/*
+			 * template t(){ void foo() { } }
+			 * t!().foo must be offered for completion
+			 */
+			if(t.Base == null)
+				isVariableInstance = true;
+
+			VisitTemplateIntermediateType(t);
+		}
+
+		public void VisitMixinTemplateType(MixinTemplateType t)
+		{
+			VisitTemplateType(t);
+		}
+
+		public void VisitEponymousTemplateType(EponymousTemplateType t)
+		{
+			if (t.Base != null)
+				t.Base.Accept(this);
+			else
+				GenUfcsAndStaticProperties(t);
+		}
+
+		public void VisitStaticProperty(StaticProperty p)
+		{
+			VisitMemberSymbol(p);
+		}
+
+		public void VisitMemberSymbol(MemberSymbol mrr)
+		{
+			if (mrr.Base != null)
+			{
+				isVariableInstance |= (mrr.Definition is DVariable && !(mrr is AliasedType) || // True if we obviously have a variable handled here. Otherwise depends on the samely-named parameter..
+						mrr.Definition is DMethod);
+
+				mrr.Base.Accept(this);
+			}
+			else
+				GenUfcsAndStaticProperties(mrr);
+		}
+
+		public void VisitTemplateParameterSymbol(TemplateParameterSymbol tps)
+		{
+			if (tps.Base != null)
+				tps.Base.Accept(this);
+			else
+			{
+				var tpp = tps.Parameter is TemplateThisParameter ? (tps.Parameter as TemplateThisParameter).FollowParameter : tps.Parameter;
+				if (tpp is TemplateTupleParameter)
+					StaticProperties.ListProperties(CompletionDataGenerator, MemberFilter, tps, true);
+			}
+		}
+
+		public void VisitModuleSymbol(ModuleSymbol tr)
+		{
+			if (isVariableInstance)
 				return;
 
-			if(rr.DeclarationOrExpressionBase is ITypeDeclaration)
-				isVariableInstance |= (rr.DeclarationOrExpressionBase as ITypeDeclaration).ExpressesVariableAccess;
-
-			if (rr is ArrayAccessSymbol || rr is DelegateCallSymbol) {
-				isVariableInstance = true;
-				rr = (rr as DerivedDataType).Base;
-			} else if (rr is AssocArrayType)
-				isVariableInstance = true;
-
-			if (rr is TemplateParameterSymbol) {
-				var tps = rr as TemplateParameterSymbol;
-				if (tps.Base == null) {
-					var tpp = tps.Parameter is TemplateThisParameter ? (tps.Parameter as TemplateThisParameter).FollowParameter : tps.Parameter;
-					if (tpp is TemplateTupleParameter) {
-						StaticProperties.ListProperties (CompletionDataGenerator, MemberFilter, tps, true);
-						return;
-					}
-				}
-			}
-
-			var mrr = rr as MemberSymbol;
-			if (mrr != null && mrr.Base != null) {
-				BuildCompletionData(mrr.Base,
-					isVariableInstance ||
-					(mrr.Definition is DVariable && !(mrr is AliasedType) || // True if we obviously have a variable handled here. Otherwise depends on the samely-named parameter..
-						mrr.Definition is DMethod),	mrr);
-			}
-
-			// A module path has been typed
-			else if (!isVariableInstance && rr is ModuleSymbol)
-				BuildCompletionData ((ModuleSymbol)rr);
-			else if (rr is PackageSymbol)
-				BuildCompletionData ((PackageSymbol)rr);
-
-			#region A type was referenced directly
-			else if (rr is EnumType) {
-				var en = (EnumType)rr;
-
-				foreach (var e in en.Definition)
-					CompletionDataGenerator.Add (e);
-				// TODO: Enlist ufcs items&stat props here aswell?
-			} else if (rr is TemplateIntermediateType) {
-				var tr = (TemplateIntermediateType)rr;
-
-				if (tr.DeclarationOrExpressionBase is TokenExpression) {
-					int token = ((TokenExpression)tr.DeclarationOrExpressionBase).Token;
-
-					isVariableInstance = token == DTokens.This || token == DTokens.Super;
-				}
-				else if (tr is TemplateType && tr.Base == null)
-				{
-					/*
-					 * template t(){ void foo() { } }
-					 * t!().foo must be offered for completion
-					 */
-					isVariableInstance = true;
-				}
-
-				// Cases:
-
-				// myVar. (located in basetype definition)		<-- Show everything
-				// this. 										<-- Show everything
-				// myVar. (not located in basetype definition) 	<-- Show public and public static members
-				// super. 										<-- Show all base type members
-				// myClass. (not located in myClass)			<-- Show all static members
-				// myClass. (located in myClass)				<-- Show all static members
-
-				BuildCompletionData (tr, isVariableInstance);
-			}
-			#endregion
-
-			else if (rr is PointerType) {
-				var pt = (PointerType)rr;
-				if (!(pt.Base is PrimitiveType && pt.Base.DeclarationOrExpressionBase is PointerDecl))
-					BuildCompletionData (pt.Base, true, pt);
-			} else {
-				if (isVariableInstance)
-					GenUfcsCompletionItems (rr);
-				StaticProperties.ListProperties (CompletionDataGenerator, MemberFilter, rr, isVariableInstance);
-			}
-		}
-
-		void BuildCompletionData(PackageSymbol mpr)
-		{
-			foreach (var kv in mpr.Package.Packages)
-				CompletionDataGenerator.AddPackage(kv.Value.Name);
-
-			foreach (var kv in mpr.Package.Modules)
-				CompletionDataGenerator.AddModule(kv.Value);
-		}
-
-		void BuildCompletionData(ModuleSymbol tr)
-		{
 			foreach (var i in tr.Definition)
 			{
 				var di = i as DNode;
@@ -166,19 +250,25 @@ namespace D_Parser.Completion.Providers
 			}
 		}
 
-		void BuildCompletionData(UserDefinedType tr, bool showInstanceItems)
+		public void VisitPackageSymbol(PackageSymbol mpr)
 		{
-			MemberCompletionEnumeration.EnumChildren(CompletionDataGenerator, ctxt, tr, showInstanceItems, MemberFilter);
-			if (showInstanceItems)
-				GenUfcsCompletionItems (tr);
-			StaticProperties.ListProperties(CompletionDataGenerator, MemberFilter, tr, showInstanceItems);
+			foreach (var kv in mpr.Package.Packages)
+				CompletionDataGenerator.AddPackage(kv.Value.Name);
+
+			foreach (var kv in mpr.Package.Modules)
+				CompletionDataGenerator.AddModule(kv.Value);
 		}
 
-		void GenUfcsCompletionItems(AbstractType t)
+		public void VisitDTuple(DTuple tps)
 		{
-			if(CompletionOptions.Instance.ShowUFCSItems)
-				foreach (var ufcsItem in UFCSResolver.TryResolveUFCS(t, 0, ed.CaretLocation, ctxt))
-					CompletionDataGenerator.Add ((ufcsItem as DSymbol).Definition);
+			GenUfcsAndStaticProperties(tps);
+		}
+
+		public void VisitAmbigousType(AmbiguousType t)
+		{
+			// Error?
+			foreach (var subType in t.Overloads)
+				subType.Accept(this);
 		}
 	}
 }
