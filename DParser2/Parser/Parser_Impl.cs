bf71cbfb8a4baa5eba5f11be973ecb64eff1026b
Orvid King <blah38621@gmail.com>
2014-02-28 08:37:29 -0600

    Fixed parsing of inline asm instructions that are also keywords.

--- DParser2/Parser/Parser_Impl.cs
+++ DParser2/Parser/Parser_Impl.cs
@@ -4138,6 +4138,8 @@ namespace D_Parser.Parser
 			var l = new List<AbstractStatement>();
 			while (!IsEOF && laKind != (CloseCurlyBrace))
 			{
+				bool retrying = false;
+			Retry:
 				bool noStatement = false;
 				if (laKind == Align)
 				{
@@ -4222,13 +4224,24 @@ namespace D_Parser.Parser
 						((AsmStatement.InstructionStatement)parentStatement).Arguments = args.ToArray();
 					else if (parentStatement is AsmStatement.RawDataStatement)
 						((AsmStatement.RawDataStatement)parentStatement).Data = args.ToArray();
-
 				}
 				else if (laKind != Semicolon)
 				{
-					noStatement = true;
-					SynErr(Identifier);
-					Step();
+					string val;
+					if (!retrying && Keywords.TryGetValue(laKind, out val))
+					{
+						la.LiteralValue = val;
+						la.Kind = Identifier;
+						Lexer.laKind = Identifier;
+						retrying = true;
+						goto Retry;
+					}
+					else
+					{
+						noStatement = true;
+						SynErr(Identifier);
+						Step();
+					}
 				}
 				else
 					Step();
