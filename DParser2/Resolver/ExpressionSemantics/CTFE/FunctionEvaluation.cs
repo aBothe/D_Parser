ad90d2dedeefeff1ee7f36a2d3fb34cd7ad7ca7f
Alexander Bothe <info@alexanderbothe.com>
2018-10-04 06:49:43 +0200

    FunctionEvaluation: Reformat.

--- DParser2/Resolver/ExpressionSemantics/CTFE/FunctionEvaluation.cs
+++ DParser2/Resolver/ExpressionSemantics/CTFE/FunctionEvaluation.cs
@@ -10,19 +10,24 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 {
 	public class CtfeException : Exception
 	{
-		public CtfeException(string msg = null) : base(msg) { }
+		public CtfeException(string msg = null) : base(msg)
+		{
+		}
 	}
 
 	public class FunctionEvaluation : StatementVisitor
 	{
 		#region Properties
+
 		readonly InterpretationContext vp;
 		ISymbolValue returnedValue;
 
 		#endregion
 
 		#region Constructor/IO
-		FunctionEvaluation(MemberSymbol method, AbstractSymbolValueProvider baseValueProvider, Dictionary<DVariable, ISymbolValue> args)
+
+		FunctionEvaluation(MemberSymbol method, AbstractSymbolValueProvider baseValueProvider,
+			Dictionary<DVariable, ISymbolValue> args)
 		{
 			vp = new InterpretationContext(baseValueProvider);
 			returnedValue = null;
@@ -32,13 +37,14 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 		}
 
 		/// <summary>
-		/// 
+		///
 		/// </summary>
 		/// <param name="dm"></param>
 		/// <param name="callArguments"></param>
 		/// <param name="baseValueProvider">Required for evaluating missing default parameters.</param>
-		public static bool AssignCallArgumentsToIC<T>(MemberSymbol mr, IEnumerable<T> callArguments, AbstractSymbolValueProvider baseValueProvider,
-			out Dictionary<DVariable,T> targetArgs, ResolutionContext ctxt = null) where T:class,ISemantic
+		public static bool AssignCallArgumentsToIC<T>(MemberSymbol mr, IEnumerable<T> callArguments,
+			AbstractSymbolValueProvider baseValueProvider,
+			out Dictionary<DVariable, T> targetArgs, ResolutionContext ctxt = null) where T : class, ISemantic
 		{
 			var dm = mr.Definition as DMethod;
 
@@ -49,7 +55,7 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 			if (TypeResolution.UFCSResolver.IsUfcsResult(mr, out firstArg) && firstArg is T)
 			{
 				var ufcsPrependedArgList = new List<T>();
-				ufcsPrependedArgList.Add((T)firstArg);
+				ufcsPrependedArgList.Add((T) firstArg);
 				ufcsPrependedArgList.AddRange(callArguments);
 				callArguments = ufcsPrependedArgList;
 			}
@@ -83,7 +89,7 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 				{
 					if (typeof(T) == typeof(AbstractType))
 						targetArgs[par] = ExpressionTypeEvaluation.EvaluateType(par.Initializer, ctxt) as T;
-					else if(typeof(T) == typeof(ISymbolValue))
+					else if (typeof(T) == typeof(ISymbolValue))
 						targetArgs[par] = Evaluation.EvaluateValue(par.Initializer, baseValueProvider) as T;
 				}
 				else
@@ -93,7 +99,8 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 			return !argEnumerator.MoveNext();
 		}
 
-		public static ISymbolValue Execute(MemberSymbol method, Dictionary<DVariable, ISymbolValue> arguments, AbstractSymbolValueProvider vp)
+		public static ISymbolValue Execute(MemberSymbol method, Dictionary<DVariable, ISymbolValue> arguments,
+			AbstractSymbolValueProvider vp)
 		{
 			if (vp.ResolutionContext.CancellationToken.IsCancellationRequested)
 				return null;
@@ -102,10 +109,10 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 
 			if (dm == null || dm.BlockStartLocation.IsEmpty)
 				return new ErrorValue(new EvaluationException("Method either not declared or undefined", method));
-			var eval = new FunctionEvaluation(method,vp,arguments);
+			var eval = new FunctionEvaluation(method, vp, arguments);
 			ISymbolValue ret;
 
-			using (vp.ResolutionContext.Push(method, dm.BlockStartLocation))
+			using (vp.ResolutionContext.Push(method))
 			{
 				try
 				{
@@ -128,27 +135,22 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 
 		public void Visit(ModuleStatement moduleStatement)
 		{
-			
 		}
 
 		public void Visit(ImportStatement importStatement)
 		{
-			
 		}
 
 		public void VisitImport(ImportStatement.Import import)
 		{
-			
 		}
 
 		public void VisitImport(ImportStatement.ImportBinding importBinding)
 		{
-			
 		}
 
 		public void VisitImport(ImportStatement.ImportBindings bindings)
 		{
-			
 		}
 
 		public void Visit(BlockStatement blockStatement)
@@ -163,52 +165,42 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 
 		public void Visit(LabeledStatement labeledStatement)
 		{
-			
 		}
 
 		public void Visit(IfStatement ifStatement)
 		{
-			
 		}
 
 		public void Visit(WhileStatement whileStatement)
 		{
-			
 		}
 
 		public void Visit(ForStatement forStatement)
 		{
-			
 		}
 
 		public void Visit(ForeachStatement foreachStatement)
 		{
-			
 		}
 
 		public void Visit(SwitchStatement switchStatement)
 		{
-			
 		}
 
 		public void Visit(SwitchStatement.CaseStatement caseStatement)
 		{
-			
 		}
 
 		public void Visit(SwitchStatement.DefaultStatement defaultStatement)
 		{
-			
 		}
 
 		public void Visit(ContinueStatement continueStatement)
 		{
-			
 		}
 
 		public void Visit(BreakStatement breakStatement)
 		{
-			
 		}
 
 		public void Visit(ReturnStatement returnStatement)
@@ -218,117 +210,94 @@ namespace D_Parser.Resolver.ExpressionSemantics.CTFE
 
 		public void Visit(GotoStatement gotoStatement)
 		{
-			
 		}
 
 		public void Visit(WithStatement withStatement)
 		{
-			
 		}
 
 		public void Visit(SynchronizedStatement synchronizedStatement)
 		{
-			
 		}
 
 		public void Visit(TryStatement tryStatement)
 		{
-			
 		}
 
 		public void Visit(TryStatement.CatchStatement catchStatement)
 		{
-			
 		}
 
 		public void Visit(TryStatement.FinallyStatement finallyStatement)
 		{
-			
 		}
 
 		public void Visit(ThrowStatement throwStatement)
 		{
-			
 		}
 
 		public void Visit(ScopeGuardStatement scopeGuardStatement)
 		{
-			
 		}
 
 		public void VisitAsmStatement(AsmStatement asmStatement)
 		{
-			
 		}
 
 		public void Visit(PragmaStatement pragmaStatement)
 		{
-			
 		}
 
 		public void Visit(StatementCondition condition)
 		{
-			
 		}
 
 		public void Visit(VolatileStatement volatileStatement)
 		{
-			
 		}
 
 		public void Visit(ExpressionStatement expressionStatement)
 		{
-			
 		}
 
 		public void Visit(DeclarationStatement declarationStatement)
 		{
-			
 		}
 
 		public void Visit(TemplateMixin templateMixin)
 		{
-			
 		}
 
 		public void Visit(DebugSpecification versionSpecification)
 		{
-			
 		}
 
 		public void Visit(VersionSpecification versionSpecification)
 		{
-			
 		}
 
 		public void Visit(StaticAssertStatement s)
 		{
-			
 		}
 
-        public void Visit(StaticForeachStatement foreachStatement)
-        {
-
-        }
-
-        public void VisitAsmInstructionStatement(AsmInstructionStatement instrStatement)
+		public void Visit(StaticForeachStatement foreachStatement)
 		{
+		}
 
+		public void VisitAsmInstructionStatement(AsmInstructionStatement instrStatement)
+		{
 		}
 
 		public void VisitAsmRawDataStatement(AsmRawDataStatement dataStatement)
 		{
-
 		}
 
 		public void VisitAsmAlignStatement(AsmAlignStatement alignStatement)
 		{
-
 		}
 
 		public void VisitMixinStatement(MixinStatement s)
 		{
-			
 		}
 	}
 }
